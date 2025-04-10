db818fd70e12af1ccf0dacc64c225038bf269d5e
tejasvi-shet <tejasvishet09@gmail.com>
2025-04-10 12:26:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ import PrivacyPolicy from './PrivacyPolicy';
 import ContactUs from './ContactUs';
 import Gallery from './components/Gallery';
 import UploadComponent from "./components/UploadComponent";
+import Dashboard from './Dashboard';  // Importing the Dashboard component
+import Coloring from './Coloring';
+import Chat from "./Chat";
+
+
 
 function App() {
   return (
@@ -23,14 +28,17 @@ function App() {
         <Route path="/hospitals" element={<HospitalSelection />} />
         <Route path="/doctors/:hospitalId" element={<DoctorSelection />} />
         <Route path="/canvas/:doctorId" element={<NoteItCanvas />} />
-        <Route path="/fundraiser" element={<Fundraiser />} /> 
+        <Route path="/fundraiser" element={<Fundraiser />} />
         <Route path="/Fundraiser" element={<Fundraiser />} />
-
+        <Route path="/dashboard" element={<Dashboard />} />  {/* Added Dashboard Route */}
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/upload" element={<UploadComponent />} />
+        <Route path="/coloring" element={<Coloring />} />
+         <Route path="/chat" element={<Chat />} />
+
       </Routes>
     </Router>
   );
